207744d94065ae137da354e4abe887ce3109814a
Avi Kadria <avi.kadria3@gmail.com>
2018-06-19 15:30:46 +0300

    finished ex4

---
+++ ImageService_android/app/src/main/java/com/example/avika/imageservice/Client.java
@@ -0,0 +1,85 @@
+package com.example.avika.imageservice;
+import android.os.Build;
+import android.util.Log;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import static java.lang.Thread.sleep;
+
+public class Client {
+
+    /**
+     * This method sends a single picture to the tcp server.
+     * @param pic image to transfer
+     */
+    public void connectToServerAndSend(File pic) throws Exception {
+        try {
+            //here you must put your computer's IP address.
+            InetAddress serverAddr = InetAddress.getByName("10.0.2.2");
+            //create a socket to make the connection with the server
+            Socket socket = new Socket(serverAddr, 2500);
+            try {
+
+                OutputStream output = socket.getOutputStream();
+                InputStream input = socket.getInputStream();
+                byte[] proveReadImage = new byte[1];
+                // write the image to the server
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    output.write(pic.toPath().getFileName().toString().getBytes());
+                }
+                int i = input.read(proveReadImage);
+                sleep(500);
+                if (i == 1) {
+                    output.write(extractBytes(pic));
+                }
+                output.flush();
+            } catch (Exception e) {
+                Log.e("TCP", "S: Error", e);
+            } finally {
+                socket.close();
+            }
+        } catch (Exception e) {
+            Log.e("TCP", "C: Error", e);
+        }
+
+    }
+
+    /**
+     * This method transforms the picture into bytes.
+     * @param file picture to transform
+     * @return array of bits of the picture
+     * @throws IOException
+     */
+    private static byte[] extractBytes(File file) throws IOException {
+        // open image
+        System.out.println(file.exists() + "!");  // prints whether the file exists.
+        FileInputStream fis = new FileInputStream(file);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];  //buffer for the picture.
+        try {
+            for (int readNum; (readNum = fis.read(buf)) != -1; ) {
+                bos.write(buf, 0, readNum);
+                //no doubt here is 0
+                /*Writes len bytes from the specified byte array starting at offset
+                off to this byte array output stream.*/
+                System.out.println("read " + readNum + " bytes,");
+            }
+        } catch (IOException ex) {  //throw exeption
+            // Logger.getLogger(ConvertImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bos.toByteArray();
+    }
+
+    /**
+     *  empty constructor
+     */
+    public Client() {
+    }
+
+}
---
+++ ImageService_android/app/src/main/java/com/example/avika/imageservice/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.avika.imageservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * This method starts the service.
+     * @param view
+     */
+    public void startService(View view) {
+        Intent intent = new Intent(this,AService.class);
+        startService(intent);
+    }
+
+    /**
+     * This method is used to set the view of the activity.
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method stops the service
+     * @param view
+     */
+    public void stopService(View view) {
+        Intent intent = new Intent(this,AService.class);
+        System.out.println("Stopping service from main activity");
+        stopService(intent);
+    }
+}
